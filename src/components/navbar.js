33f98ed1201fdf73fc1c5d50b2ffea56766b23c1
Marcelo D'andrea <ke_lo8@hotmail.com>
2019-11-15 21:03:22 -0300

    Arrrglo de un ClassName (Home)

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,7 +14,7 @@ class NavBar extends React.Component {
           <ul className="navbar-nav mr-auto">
             <li classNmae="nav-item active">
               <a className="nav-link" href="#">
-                Home <span class="sr-only">(current)</span>
+                Home <span className="sr-only">(current)</span>
               </a>
             </li>
             <li className="nav-item">
